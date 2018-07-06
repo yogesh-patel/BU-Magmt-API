31bffad779d5ca30455a22c248cb63712bc4328b
Amruta Mundkar <amrutam@synerzipune.local>
2018-07-06 17:42:43 +0530

    Clean master branch

--- src/main/java/com/synerzip/auth/userRepository/ApplicationUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.synerzip.auth.userRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.synerzip.auth.model.ApplicationUser;
-
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
-    ApplicationUser findByUsername(String username);
-}
--- src/main/java/com/synerzip/service/BillingStatusService.java
+++
@@ -1,9 +0,0 @@
-package com.synerzip.service;
-
-public interface BillingStatusService {
-
-	public Long getStatusCount(String string);
-
-	//Long getBillableCount(String string);
-
-}
